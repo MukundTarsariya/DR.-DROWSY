83d5bfed2fa89dcd2b5fd4d760f3bb49a92c7862
Mukund Tarsariya <mukundtarsariya@loyalistcollege.com>
2022-03-04 13:54:27 -0500

    Checking In the Project Directory

--- myDrowsinessDetectionApp/views.py
+++ myDrowsinessDetectionApp/views.py
@@ -2,12 +2,15 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.http import HttpResponse
 
+#Dashborad page route
 def index_show(request):
    return render(request, 'index.html')
 
+#Login page route
 def pagelogin_show(request):
    return render(request, 'page_login.html')
 
+#SignUp page route
 def pageregister_show(request):
    return render(request, 'page_register.html')
 
